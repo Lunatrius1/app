39293b2eaf2d85123d5811bba0aba6274cbf236b
Lunatrius1 <49868481+Lunatrius1@users.noreply.github.com>
2020-01-15 23:01:18 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from tkinter import *
+from threading import Thread
+
+
+
+
+
+
+
+
+root = Tk()
+root.geometry("500x500")
+root.config(background="light green")
+
+test = Label(root, text="Test")
+test.pack()
+
+root.mainloop()
